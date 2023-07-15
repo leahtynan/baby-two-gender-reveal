0ae334e9d93cc79ddb97c8372069047dc45d4362
Leah Tynan <brunetto@alum.mit.edu>
2023-07-15 16:14:24 -0400

    Set up file structures and layout

---
+++ scripts.js
@@ -0,0 +1,18 @@
+// Baby Two Gender Reveal by Leah Tynan 2023
+
+// -------------------- BUTTONS --------------------
+
+var boyButton = document.getElementById("boy-button"); 
+boyButton.addEventListener("click", function () { 
+  // TODO: Choose boy
+});
+
+var girlButton = document.getElementById("girl-button"); 
+girlButton.addEventListener("click", function () { 
+	// TODO: Choose girl
+});
+
+var resetButton = document.getElementById("reset-button"); 
+resetButton.addEventListener("click", function () { 
+	// TODO: Reset
+});                     
