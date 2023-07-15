89d6eec0fed36e6fe1d1c8d4f06514276b335a5c
Leah Tynan <brunetto@alum.mit.edu>
2023-07-15 16:50:45 -0400

    Added interactability, UI changes, and UI reset on body load

--- scripts.js
+++ scripts.js
@@ -4,38 +4,51 @@
 
 var boyButton = document.getElementById("boy-button"); 
 boyButton.addEventListener("click", function () { 
-  // TODO: Choose boy
+	guessBoy();
 });
 
 var girlButton = document.getElementById("girl-button"); 
 girlButton.addEventListener("click", function () { 
-	// TODO: Choose girl
+	guessGirl();
 });
 
 var resetButton = document.getElementById("reset-button"); 
 resetButton.addEventListener("click", function () { 
-	// TODO: Reset
+	reset();
 });                     
 
 // -------------------- FUNCTIONS -------------------
 
 function reset() {
 // *** Restarts the interaction ***
-	// TODO: Change background color back to white
-	// TODO: Bring girl column back to full opacity
-	// TODO: Bring back "Make your guess!" to helper text container
-	// TODO: Fade "Reset" button and make it non-interactable
+	document.body.style.background = "white";
+	document.getElementById("helper-text").innerHTML = "Make your guess!";
+	resetButton.style.opacity = "0.3";
+	resetButton.disabled = "disabled";
+	boyButton.removeAttribute("disabled");
+	girlButton.removeAttribute("disabled");
+	boyButton.style.opacity = "1.0";
+	girlButton.style.opacity = "1.0";
 }
 
 function guessBoy() {
 	// *** Shows boy messaging and reveals answer *** /
-	// TODO: Change background color to blue
+	document.getElementById("helper-text").innerHTML = "Yes, it's a boy this time!";
+	showAnswer();
+}
+
+function guessGirl() {
+	// *** Shows girl messaging and reveals answer *** /
+	document.getElementById("helper-text").innerHTML = "Nope, it's a boy this time!";
+	showAnswer();
 }
 
 function showAnswer() {
-	// ** Updates UI to show the answer */
-	// TODO: Change background color to blue
-	// TODO: Dims girl button
-	// TODO: Make both girl and boy buttons non-interactable
-	// TODO: Un-fade "Reset" button and make it interactable
+	// ** Updates UI to show the answer **
+	document.body.style.background = "#2acaea";
+	girlButton.style.opacity = "0.3";
+	boyButton.disabled = "disabled";
+	girlButton.disabled = "disabled";
+	resetButton.style.opacity = "1.0";
+	resetButton.removeAttribute("disabled");
 }
