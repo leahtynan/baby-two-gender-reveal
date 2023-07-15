f5e0625670ba518e997c5dbe9625bc8fee9c5917
Leah Tynan <brunetto@alum.mit.edu>
2023-07-15 16:26:53 -0400

    Added reset button, responsiveness for main container, function comments

--- scripts.js
+++ scripts.js
@@ -16,3 +16,26 @@ var resetButton = document.getElementById("reset-button");
 resetButton.addEventListener("click", function () { 
 	// TODO: Reset
 });                     
+
+// -------------------- FUNCTIONS -------------------
+
+function reset() {
+// *** Restarts the interaction ***
+	// TODO: Change background color back to white
+	// TODO: Bring girl column back to full opacity
+	// TODO: Bring back "Make your guess!" to helper text container
+	// TODO: Fade "Reset" button and make it non-interactable
+}
+
+function guessBoy() {
+	// *** Shows boy messaging and reveals answer *** /
+	// TODO: Change background color to blue
+}
+
+function showAnswer() {
+	// ** Updates UI to show the answer */
+	// TODO: Change background color to blue
+	// TODO: Dims girl button
+	// TODO: Make both girl and boy buttons non-interactable
+	// TODO: Un-fade "Reset" button and make it interactable
+}
