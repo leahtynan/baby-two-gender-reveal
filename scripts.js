9a9a9c55757fc7e523ec85aad6046452d8d2a7dd
Leah Tynan <brunetto@alum.mit.edu>
2023-07-17 09:17:41 -0400

    Added art assets, adjusted colors and text styling

--- scripts.js
+++ scripts.js
@@ -23,6 +23,8 @@ function reset() {
 // *** Restarts the interaction ***
 	document.body.style.background = "white";
 	document.getElementById("helper-text").innerHTML = "Make your guess!";
+	document.getElementById("helper-text").style.color = "black";
+	document.getElementById("helper-text").style.fontWeight = "lighter";
 	resetButton.style.opacity = "0.15";
 	resetButton.disabled = "disabled";
 	boyButton.removeAttribute("disabled");
@@ -45,7 +47,9 @@ function guessGirl() {
 
 function showAnswer() {
 	// ** Updates UI to show the answer **
-	document.body.style.background = "#2acaea";
+	document.body.style.background = "#0072bb";
+	document.getElementById("helper-text").style.color = "white";
+	document.getElementById("helper-text").style.fontWeight = "bolder";
 	girlButton.style.opacity = "0.15";
 	boyButton.disabled = "disabled";
 	girlButton.disabled = "disabled";
